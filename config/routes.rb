f13e1c26fc330958d6ff0dae8eeb53695e0072b1
andy2000xyz <andy2000xyz@hotmail.com>
2017-02-20 21:07:46 +0800

    add admin/orders/state_option button

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
   root 'products#index'
   devise_for :users
+
   namespace :admin do
-      resources :products do
-      resources :orders  
-        member do
-          post :add_to_cart
-        end
-      end
+    resources :products
+    resources :orders do
+      member do
+        post :cancel
+        post :ship
+        post :shipped
+        post :return
+     end
+   end
   end
 
   resources :products do
@@ -17,12 +21,11 @@ Rails.application.routes.draw do
   end
 
   resources :carts do
-    collection do
+   collection do
       delete :clean
       post :checkout
     end
   end
-
 
   resources :cart_items
 
@@ -30,18 +33,12 @@ Rails.application.routes.draw do
     member do
       post :pay_with_alipay
       post :pay_with_wechat
+      post :apply_to_cancel
     end
   end
 
-   namespace :account do
-   resources :orders
+  namespace :account do
+    resources :orders
   end
-
   root 'welcome#index'
-  resources :carts
-
 end
-
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
