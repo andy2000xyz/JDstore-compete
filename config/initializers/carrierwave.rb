53372e11899cb6de106aca9ceebac8ec7d6c9945
andy2000xyz <andy2000xyz@hotmail.com>
2018-06-05 17:22:49 +0800

    amend carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,19 +1,19 @@
 CarrierWave.configure do |config|
   if Rails.env.production?
-    config.fog_provider = "fog"
+    config.storage :fog
     config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     ENV["AKIAIOE5M2ROO4P5PZDQ"],      # 你的 key
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],      # 你的 key
 
 
-      aws_secret_access_key: ENV["yvwN2pv9fOa5/t7ibMRGb78dJlv7QZy4RpvWbdkM"],      # 你的 secret key
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],      # 你的 secret key
 
 
       region:                'ap-northeast-1' # 你的 S3 bucket 的 Region 位置
 
 
     }
-    config.fog_directory  = ENV["alex-jdstore"] # 你设定的 bucket name
+    config.fog_directory  = ENV["AWS_BUCKET_NAME"] # 你设定的 bucket name
 
 
 
