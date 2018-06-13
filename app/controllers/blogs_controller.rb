2f2ecb83d361637fff839496a279a2864b71925d
andy2000xyz <andy2000xyz@hotmail.com>
2018-06-14 00:54:46 +0800

    blog show页面增加上一页、下一页功能

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,9 +1,12 @@
 class BlogsController < ApplicationController
   def index
-    @blogs = Blog.all
+    @blogs = Blog.rank(:row_order).all
   end
 
   def show
     @blog = Blog.find(params[:id])
+    @previous = @blog.previous_blog
+    @next = @blog.next_blog
+    # @blog_list =@blog.blog_list
   end
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,23 @@
 class Blog < ApplicationRecord
   include RankedModel
   ranks :row_order
+
+  # def blog_list
+  #   blogs = Blog.all
+  #   blog_list = blogs.all.order('created_at DESC').first
+  # end
+
+  def previous_blog
+    previous_blogs = Blog.where [ 'created_at < ?', self.created_at ]
+    previous_blog = previous_blogs.all.order('created_at DESC').first
+
+    previous_blog.nil? ? Blog.all.order('created_at DESC').first : previous_blog
+  end
+
+  def next_blog
+    next_blogs = Blog.where [ 'created_at > ?', self.created_at ]
+    next_blog = next_blogs.all.order('created_at ASC').first
+
+    next_blog.nil? ? Blog.all.order('created_at ASC').first : next_blog
+  end
 end
